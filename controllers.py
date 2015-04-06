0a4bf4b06b97759f35efb04217acfaafd263af51
Tobias Wetzel <tobi.wetzel@web.de>
2015-04-06 15:56:24 +0200

    UI polishing

--- controllers.py
+++ controllers.py
@@ -12,8 +12,37 @@ class EventParticipant(http.Controller):
         cr, uid, context = request.cr, request.uid, request.context
 
         values = {}
-        values["meals"] = {"normal": {"cost": 0.00}, "vegan": {"cost", 20.00}, "diet": {"cost": 10.50}}
-        values["tracks"] = [{"name": "security", "minitracks": [{"name": "cybersecurity", "date": "xyz", "duration": "5"}, "information security"]}, {"name": "social media", "minitracks": ["introduction", "blablabla"]}, {"name": "big data", "minitracks": ["analisys", "so cool"]}, {"name": "innovation", "minitracks": ["brand new"]}, {"name": "research", "minitracks": []}]
+        values["meals"] = {"Vegetarian": {"cost": 0.00}, "Vegan": {"cost", 20.00}, "Wheat/Gluten-free": {"cost": 10.50}}
+        #values["tracks"] = [{"name": "security", "minitracks": [{"name": "cybersecurity", "date": "xyz", "duration": "5"}, "information security"]}, {"name": "social media", "minitracks": ["introduction", "blablabla"]}, {"name": "big data", "minitracks": ["analisys", "so cool"]}, {"name": "innovation", "minitracks": ["brand new"]}, {"name": "research", "minitracks": []}]
+
+        values["tracks"] =  [{"name": "Security",
+					"events_full_day": [{"name": "Rapid Screening Technologies, Deception, Detection and Credibility Assessment (2 days) (S) "}],
+					"events_slots":[{"morning": "Cybersecurity in Action (S)", "afternoon": "information security"}]},
+					{"name": "Big Data",
+					"events_full_day": [{"name": "Real-Time Big Data Analytics (W)"}],
+					"events_slots": [{"morning": "Introduction to Big Data and Its Technology (T)", "afternoon": "Big Data  Analytics (T)"},
+									{"morning": "Introduction to Text Mining in Virtual Team Research (T)", "afternoon": "Visual Mining and Analysis of Social Media (T)"},
+									{"morning": "", "afternoon": "Business Analytics and Big Data: Resources for Next Generation of Knowledge Workers (W)"}]},
+					{"name": "Social Media & Networks",
+					"events_full_day": [{"name": "Social Media Research: Tools and Methods / Results from Analyses of Social Media Data; Continuing Challenges (W)"}],
+					"events_slots": [{"morning": "Introduction to Social Network Analysis (T)", "afternoon": "Collective Intelligence and Crowdsourcing (W)"},
+									{"morning": "Community Systems Design (T)", "afternoon": "Cross Cultural Communication in Accessible Global Virtual Teams (W)"}]},
+					{"name": "Innovation & Sustainability",
+					"events_full_day": [{"name": "Emerging Technical Frontiers for Service Innovation (S)"},
+                                        {"name": "Innovating Together: Co-creation and Co-production of Public Services (S)"},
+                                        {"name": "Sustainable Energy and Computing (S)"}],
+					"events_slots": [{"morning": "Information Technology and Innovation (S)", "afternoon": "Wireless Innovation and Services Beyond 2020 (W)"}]},
+					{"name": "E-Health",
+					"events_full_day": [{"name": "Learning Health Systems (1.5 days) (S)"}],
+					"events_slots": []},
+					{"name": "Teaching & Research Methods",
+					"events_full_day": [],
+					"events_slots":[{"morning": "Philosophy of Technology for Information Systems Scholars (T)", "afternoon": "Interaction Design for Specifying Business Requirements (T)"},
+                                    {"morning": "Cloud Computing and Decision Analytics (T)", "afternoon": "Teaching with Tableau (W)"},
+                                    {"morning": "Biomedical/Clinical Natural Language Processing (T)", "afternoon": "Get Your First Experience with Serious Games and Game Design Thinking"},
+                                    {"morning": "Academic Integrity Research: Can We Change the Culture? (W)", "afternoon": "Design Science in Action: Exemplars from Information Systems Privacy Artifacts in IS Research (S)"},
+									{"morning": "Writing Review and Meta-analysis Papers in Information Systems (W)", "afternoon": ""}]}
+					]
 
         return request.website.render("event_participation.all_the_info", values)
 
--- static/src/js/track_selection.js
+++ static/src/js/track_selection.js
@@ -2,15 +2,33 @@
  * Created by Tobias on 02.04.2015.
  */
 
+function mergeNodeLists(list1, list2){
+    var array1 = Array.prototype.slice.call(list1);
+    var array2 = Array.prototype.slice.call(list2);
+
+    return array1.concat(array2);
+}
+
 function selectTrack(element){
 
     var selected = (element.className.indexOf("selected") == -1 ? 0 : 1);
-    var tracks = document.getElementsByClassName("track-selectable");
+    var track_class = element.className;
+
+    if (track_class.indexOf("morning") > -1) {
+        var tracks = mergeNodeLists(document.getElementsByClassName("track-selectable morning"), document.getElementsByClassName("track-selectable full_day"));
+    }
+    else if (track_class.indexOf("afternoon") > -1) {
+        var tracks = mergeNodeLists(document.getElementsByClassName("track-selectable afternoon"), document.getElementsByClassName("track-selectable full_day"));
+    }
+    else {
+        var tracks = document.getElementsByClassName("track-selectable");
+    }
 
 
     for(var i = 0; i < tracks.length; i++){
-        //reset all class names
-        tracks[i].className= "track-selectable";
+
+        if(tracks[i].className.indexOf("selected") > -1)
+            tracks[i].className = tracks[i].className.replace("selected","");
     }
 
     //if element has not allready been selected
